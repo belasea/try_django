2ba93ca4edff2cb3e102f254656437697a319e2c
jibontech <jibon.belaface@gmail.com>
2025-05-12 16:15:19 +0600

    initial commit

---
+++ try_django/local_settings.py
@@ -0,0 +1,47 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-tszl_bh%$@@fr@8n8&8zr8i6l9@%np8^17b!3-sg@4x8w2@h91'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+BASE_URL = "*"
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'try_django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# Added Static Setting For local Machine
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "Upload", "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "Upload", "media")
