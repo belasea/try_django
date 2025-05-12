732a36ef60594031b7364a13cf5f761b2df441c6
jibontech <jibon.belaface@gmail.com>
2025-05-12 17:11:44 +0600

    added secret key

--- try_django/settings.py
+++ try_django/settings.py
@@ -16,6 +16,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-tszl_bh%$@@fr@8n8&8zr8i6l9@%np8^17b!3-sg@4x8w2@h91'
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
