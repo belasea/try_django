e5f8e2b5f331ae1d3066b11f30037f07d323af5d
jibontech <jibon.belaface@gmail.com>
2025-05-12 16:56:23 +0600

    added home app

---
+++ home/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HomeViewTests(TestCase):
+    def test_home_view_returns_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello World !")
